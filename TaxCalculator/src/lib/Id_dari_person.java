4b604a4adac0bf8d2d621965f5ca3d0e15d43970
nikoofficial <anikorizki41@gmail.com>
2023-04-14 22:06:04 +0700

    updated_class
    update dari class

---
+++ TaxCalculator/src/lib/Id_dari_person.java
@@ -0,0 +1,11 @@
+package lib;
+
+public class Id_dari_person {
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+
+}
